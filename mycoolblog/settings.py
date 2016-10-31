a8340c31e4d71f93062d5968d3f2518ba41d776c
Dmitry Gorbachev <gorbachevdmitr@gmail.com>
2016-11-01 01:14:53 +0300

    add true

--- mycoolblog/settings.py
+++ mycoolblog/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'yb4(7)dj)lk!$6$3lh=vu$u!a38(ut!)80wwc%oio!j&8@wgp+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = [
-]
+ALLOWED_HOSTS = []
 
 
 # Application definition
